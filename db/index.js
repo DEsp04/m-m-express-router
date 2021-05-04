8484094b73176a707e0daadafd1a173eb9467aa2
david espinal <despinal0425@gmail.com>
2021-05-04 15:35:38 -0400

    added the URI from the database atlas

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || `mongodb+srv://tuli88:${process.env}@expressrouter.un3yb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true`;
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || process.env.DB_Cluster_Password;
 
 
 
