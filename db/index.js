5bad50543832f5439b7ba7d75507aa517fb03462
david espinal <despinal0425@gmail.com>
2021-05-04 09:50:41 -0400

    fix: refactor hard coded connection string to Heroku config var

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require("dotenv").config();
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantsDatabase'
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || `mongodb+srv://tuli88:${process.env}@expressrouter.un3yb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true`;
+
+
 
 
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
@@ -10,3 +13,6 @@ mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true 
 })
 const db = mongoose.connection
 module.exports = db
+
+
+
