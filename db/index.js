b95a173e8086b850daa0e295c6bdf14a33775a06
david espinal <despinal0425@gmail.com>
2021-05-05 12:30:26 -0400

    added db url to the db index

--- db/index.js
+++ db/index.js
@@ -1,18 +1,28 @@
+//implement mongoose
 const mongoose = require('mongoose');
 require("dotenv").config();
 
+//we will be working with a certain database
+//use mongoose to establish a connection with mongodb
 let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || process.env.DB_Cluster_Password;
 
 
 
-
+//we will call upon mongoose and use the connect method to connect to the mongodb
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
   console.log('Successfully connected to MongoDB')
 }).catch(e => {
   console.error('Connection error', e.message)
 })
+
+
 const db = mongoose.connection
 module.exports = db
 
+
+//with mongodb we dont need a schema, however, we need a structure using mongoose
+
+//do npm init
+//npm install mongoose
 
 
--- models/plant.js
+++ models/plant.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
+//implement schema which mongoose offers
+//we will always be creating a schema in our model ***remember
 const Schema = mongoose.Schema
+
+
+
+
+//we create a variable and equate it to the new schema which is a blueprint
 const Plant = new Schema(
   {
     name: { type: String, required: true },
@@ -8,6 +15,9 @@ const Plant = new Schema(
   },
   {timestamps: true},
 )
+
+
+
 module.exports = mongoose.model('plants', Plant)
 
 
